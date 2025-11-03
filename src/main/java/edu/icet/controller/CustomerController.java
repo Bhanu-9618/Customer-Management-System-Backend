41d9f8f577cb5391037d7ab1b65c410a73f7bbf0
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-11-03 15:18:11 +0530

    create customer get age APi

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -1,13 +1,20 @@
 package edu.icet.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
 
-    @GetMapping
+    @GetMapping("/name")
     public String getName(){
         return "saman";
+    }
+
+    @GetMapping("/age")
+    public Integer getAge(){
+        return 18;
     }
 }
